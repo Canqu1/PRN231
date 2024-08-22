56881074c94f06562d027f8c7365b8005986b9c8
sonn2611 <hn232324@gmail.com>
2024-08-22 23:27:09 +0700

    update

--- Project/FrontEnd/Program.cs
+++ Project/FrontEnd/Program.cs
@@ -1,4 +1,4 @@
-namespace FrontEnd
+﻿namespace FrontEnd
 {
     public class Program
     {
@@ -9,6 +9,20 @@ namespace FrontEnd
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddHttpClient();
+
+            builder.Services.AddAuthentication("CookieAuthentication")
+           .AddCookie("CookieAuthentication", options =>
+           {
+               options.LoginPath = "/Login";
+               //options.AccessDeniedPath = "/Forbidden";
+               options.ExpireTimeSpan = TimeSpan.FromHours(23).Add(TimeSpan.FromMinutes(50));
+           });
+            builder.Services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20); // Thời gian hết hạn
+                options.Cookie.HttpOnly = true; // Cookie chỉ có thể được truy cập qua HTTP
+                options.Cookie.IsEssential = true; // Cookie là cần thiết cho phiên làm việc
+            });
 
 
             var app = builder.Build();
@@ -23,6 +37,7 @@ namespace FrontEnd
             app.UseRouting();
             app.UseAuthentication(); // Add if authentication is needed
             app.UseAuthorization();
+            app.UseSession();
 
             app.MapRazorPages();
 
